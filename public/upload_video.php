6bee9af60e69a2b6e317d0a3ed43d84f93052c59
Daly2025 <lydiafdez84@gmail.com>
2025-06-06 19:58:58 +0200

    arreglo css

--- public/upload_video.php
+++ public/upload_video.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Subir Video</title>
-    <link rel="stylesheet" href="css/index.css" />
+    <link rel="stylesheet" href="css/upload.css" />
 </head>
 <body>
     <div class="container">
