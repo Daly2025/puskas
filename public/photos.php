1956e53159579cbf81a4c27e8e7dd93aa0e6dabb
Daly2025 <lydiafdez84@gmail.com>
2025-06-10 18:07:06 +0200

    div iguales

--- public/photos.php
+++ public/photos.php
@@ -29,6 +29,7 @@ try {
     <title>Galería de Fotos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/photos.css">
 </head>
 <body>
     <div class="container mt-5">
